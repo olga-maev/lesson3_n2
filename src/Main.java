ac3d6792d8bb6242b8749c63931f0d181ddb79d3
olga-maev <kurumoch0@gmail.com>
2022-05-30 10:28:40 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Укажите количество денег на счете.");
         double lastMoney = scanner.nextDouble();
-        System.out.println("Укажите сумму пополнения счета." );
+        System.out.println("Укажите сумму пополнения счета.");
         double newMoney = scanner.nextDouble();
-        int moneyBonus =0;
+        int moneyBonus = 0;
         if ((int) newMoney > 1000) {
-            moneyBonus = (int) newMoney/100;};
+            moneyBonus = (int) newMoney / 100;
+        }
+        ;
         double newSchet;
-        newSchet=lastMoney+newMoney+moneyBonus;
-        System.out.println("Ваш итоговый счет "+ newSchet);
-        System.out.println("Вам зачислено "+ moneyBonus+" бонусных рублей.");
+        newSchet = lastMoney + newMoney + moneyBonus;
+        System.out.println("Ваш итоговый счет " + newSchet);
+        System.out.println("Вам зачислено " + moneyBonus + " бонусных рублей.");
 
     }
 }
